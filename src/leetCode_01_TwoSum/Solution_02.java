476ef0a0bde50d11a59a3664aabf7af33e10c844
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2019-10-25 17:32:51 +1030

    BugFix: j can not equal i;

--- src/leetCode_01_TwoSum/Solution_02.java
+++ src/leetCode_01_TwoSum/Solution_02.java
@@ -8,7 +8,7 @@ public class Solution_02
 	{
 		for (int i = 0; i < nums.length; i++)
 		{
-			for (int j = 0; j < nums.length; j++)
+			for (int j = i + 1; j < nums.length; j++)
 			{
 				if (target == nums[i] + nums[j])
 				{
